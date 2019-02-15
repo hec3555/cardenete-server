2608bde0c7af785e46d4799256c18a79cbfb3154
a004631408j <a004631408j@s09-pc16>
2019-02-15 17:00:35 +0100

    Leer -->
    Añadida clase Main, con spring boot para evitar problemas al
    desplegar (a jesus no le iba e implementando esta clase le funciona). De
    momento no la usaré porque no quiero usar spring boot a no ser que no
    quede más remedio

---
+++ cardenete-server/src/main/java/org/cardenete/config/Main.java
@@ -0,0 +1,14 @@
+package org.cardenete.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+
+@SpringBootApplication(exclude=HibernateJpaAutoConfiguration.class)
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+}
